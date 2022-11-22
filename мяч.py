ce08f3e55d43a5f8e2b8e01ed66c0645d0793824
13pechenek <sdzav@yandex.ru>
2022-11-22 10:40:34 +0300

    modified:   "\320\274\321\217\321\207.py"

--- мяч.py
+++ мяч.py
@@ -1,11 +1,10 @@
-import pygame
-from pygame.draw import *
 from random import randint
+from pygame.draw import *
+import pygame
 
 pygame.init()
 
 FPS = 0.5
-s1=False
 screen = pygame.display.set_mode((1200, 900))
 RED = (255, 0, 0)
 BLUE = (0, 0, 255)
@@ -17,82 +16,112 @@ BLACK = (0, 0, 0)
 COLORS = [RED, BLUE, YELLOW, GREEN, MAGENTA, CYAN]
 
 
-def new_ball(x, y, r, color):
-    '''рисует новый шарик '''
-    circle(screen, color, (x, y), r)
+def draw_ball(x_b, y_b, r_b, color):
+    """
+        Рисует новый шарик:
+            x_b - координата по оси x;
+            y_b - координата по оси y;
+            r_b - радиус шарика;
+            color - цвет шарика.
+    """
+    circle(screen, color, (x_b, y_b), r_b)
 
 
-def click(event):
-    o = False
-    for i in range(len(X)):
-        if (event.pos[0]-X[i])**2+(event.pos[1]-Y[i])**2<R[i]**2:
-            o = True
-            if i == len(X) - 1 and s == True:
-                s1 = True
-    return o, s1
+def click(eve):
+    """
+        Проверяет попадание по шарику
+    """
+    click_in_target = False
+    s_1 = False
+    for count_1 in enumerate(X):
+        if (eve.pos[0]-X[count_1])**2+(eve.pos[1]-Y[count_1])**2<R[i]**2:
+            click_in_target = True
+            if count_1 == len(X) - 1 and S is True:
+                s_1 = True
+    return click_in_target, s_1
+
+
+def gen_balls():
+    """
+        Заполняет массивы скоростей и координат произвольными значениями
+    """
+    global S
+    for count_2 in range(i):
+        X.append(randint(100, 1000))
+        Y.append(randint(200, 700))
+        R.append(randint(10,100))
+        VX.append(randint(-5, 5))
+        VY.append(randint(-5, 5))
+        C.append(COLORS[randint(0, 5)])
+        if randint(1, 10) == 1:
+            S = True
+    if S is True:
+        X.append(randint(100, 1000))
+        Y.append(randint(200, 700))
+        R.append(randint(10, 100))
+        VX.append(randint(-5, 5))
+        VY.append(randint(-5, 5))
+        global VR, VR1
+        VR = R[i]/600
+        VR1 = 0
+
+
+def move_balls():
+    """
+        Функция перемещает шары в зависимости от скоростей и соударений со стенами
+    """
+    global VR,  VR1
+    if S is True:
+        draw_ball(X[i], Y[i], R[i], COLORS[randint(0, 5)])
+        if X[i] + R[i] + VX[i] >= 1200 or X[i] - R[i] + VX[i] <= 0:
+            VX[i] =- VX[i]
+        elif Y[i] + R[i] + VY[i] >= 900 or Y[i] - R[i] + VY[i] <= 15:
+            VY[i] =- VY[i]
+        X[i] += VX[i]
+        Y[i] += VY[i]
+        VR1 += VR
+        if VR1 >= 1:
+            R[i] -= VR1
+            VR1 -= 1
+    for count_3 in range(i):
+        draw_ball(X[count_3], Y[count_3], R[count_3], C[count_3])
+    for count_4 in range(i):
+        if X[count_4] + R[count_4] + VX[count_4] >= 1200 or X[count_4] - R[count_4] + VX[count_4] <= 0:
+            VX[count_4] =- VX[count_4]
+        elif Y[count_4] + R[count_4] + VY[count_4] >= 900 or Y[count_4] - R[count_4] + VY[count_4] <= 15:
+            VY[count_4] =- VY[count_4]
+        X[count_4] += VX[count_4]
+        Y[count_4] += VY[count_4]
+
 
 pygame.display.update()
 clock = pygame.time.Clock()
-finished = False
-score=0
-while not finished:
-    X=[]
-    Y=[]
-    R=[]
-    VX=[]
-    VY=[]
-    C=[]
-    s=False 
-    i=30
-    for j in range(i):
-        X.append(randint(100, 1000))
-        Y.append(randint(200, 700))
-        R.append(randint(10,100))
-        VX.append(randint(-5, 5))
-        VY.append(randint(-5, 5))
-        C.append(COLORS[randint(0,5)])
-        if randint(1,10)==1:
-            s=True
-    if s ==True:
-        X.append(randint(100, 1000))
-        Y.append(randint(200, 700))
-        R.append(randint(10,100))
-        VX.append(randint(-5, 5))
-        VY.append(randint(-5, 5))
-        vr=R[i]/600
-        vr1=0
-    for k in range(600):
+FINISHED = False
+SCORE=0
+while not FINISHED:
+    X = []
+    Y = []
+    R = []
+    VX = []
+    VY = []
+    C = []
+    S = False
+    i = 30
+    VR = 0
+    VR1 = 0
+    gen_balls()
+    for c in range(600):
         clock.tick(120)
-        if s==True:
-            new_ball(X[i],Y[i],R[i],COLORS[randint(0,5)]) 
-            if X[i]+R[i]+VX[i]>=1200 or X[i]-R[i]+VX[i]<=0:
-                VX[i]=-VX[i]
-            elif Y[i]+R[i]+VY[i]>=900 or Y[i]-R[i]+VY[i]<=15:
-                VY[i]=-VY[i]
-            X[i]+=VX[i]
-            Y[i]+=VY[i]
-            vr1+=vr
-            if vr1>=1:
-                R[i]-=vr1
-                vr1-=1
-        for j in range(i):
-            new_ball(X[j],Y[j],R[j],C[j])
-        for j in range(i):
-            if X[j]+R[j]+VX[j]>=1200 or X[j]-R[j]+VX[j]<=0:
-                VX[j]=-VX[j]
-            elif Y[j]+R[j]+VY[j]>=900 or Y[j]-R[j]+VY[j]<=15:
-                VY[j]=-VY[j]
-            X[j]+=VX[j]
-            Y[j]+=VY[j]
+        move_balls()
         pygame.display.update()
         screen.fill(BLACK)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                finished = True
+                FINISHED = True
             elif event.type == pygame.MOUSEBUTTONDOWN:
-                if click(event)[0]==True:
-                    score+=100
-                    if click(event)[1]==True:
-                        score+=900
-                    print(score)
+                if click(event)[0] is True:
+                    SCORE += 100
+                    if click(event)[1] is True:
+                        SCORE += 900
+                    print(SCORE)
 pygame.quit()
